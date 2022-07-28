8ac1e8f882241b555cc3b0b3f3cd4a3a0b6f5e3f
Kevin Hirsig <khirsig@1-B-8.42heilbronn.de>
2022-07-28 08:59:47 +0200

    added stack base

---
+++ include/stack.hpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khirsig <khirsig@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 08:43:51 by khirsig           #+#    #+#             */
+/*   Updated: 2022/07/28 08:59:36 by khirsig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STACK_HPP
+# define STACK_HPP
+
+# include "vector.hpp"
+
+namespace ft {
+	template <class T, class Container = ft::vector<T> >
+		class stack {
+			public:
+				typedef Container							container_type;
+				typedef typename Container::value_type		value_type;
+				typedef typename Container::size_type		size_type;
+				typedef typename Container::reference		reference;
+				typedef typename Container::const_reference	const_reference;
+
+				explicit stack(const container_type &ctnr = container_type())
+				{
+					_c = ctnr;
+				}
+
+				bool	empty() const
+				{
+					return (_c.empty());
+				}
+
+				size_type	size() const
+				{
+					return (_c.size());
+				}
+
+				value_type	&top()
+				{
+					return (_c.back());
+				}
+
+				const value_type	&top() const
+				{
+					return (_c.back());
+				}
+
+				void	push(const value_type &val)
+				{
+					_c.push_back(val);
+				}
+
+				void	pop()
+				{
+					_c.pop_back();
+				}
+
+			private:
+				container_type	_c;
+		};
+}
+
+
+#endif
